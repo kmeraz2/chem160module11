bacbb0a9e4cc2a20e5ee04621e3a9ff3efda1aba
kmeraz2 <54649455+kmeraz2@users.noreply.github.com>
2019-10-20 20:28:02 -0700

    Add files via upload

---
+++ diabetes.R
@@ -0,0 +1,4 @@
+diabetes <- read.table("diabetes.txt",header=T) 
+model<-lm(glucose~.,data=diabetes) step(model) model<-lm(glucose~1,data=diabetes) 
+step(model,direction="forward", 
+	scope=list(upper=terms(glucose~.,data=diabetes))) 
